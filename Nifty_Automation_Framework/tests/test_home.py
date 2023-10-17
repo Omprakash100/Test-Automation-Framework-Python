58eb68c7d482808a6a28d0ad1fb04df4a75e2929
Omprakash <Omprakash_Choudhari@epam.com>
2023-10-17 12:27:59 +0530

    Automation Exercise Framework Python updated assert

--- Nifty_Automation_Framework/tests/test_home.py
+++ Nifty_Automation_Framework/tests/test_home.py
@@ -24,10 +24,10 @@ class TestHome:
         sorted_company_profit_percentage = sort_the_dictionary(company_profit_percentage)
         top_five_stocks = sorted_company_profit_percentage[:5]
         shared_top5_stocks.update(dict(top_five_stocks))
-        logger.info(f"Top five stocks sorted manually {top_five_stocks}")
+        logger.info(f"Top five stocks sorted manually {shared_top5_stocks}")
 
     def test_verify_top5_stocks(self, shared_top5_stocks, home_page):
         home_page.sort_by_clicking()
         top_five_stocks = home_page.fetch_top5_stocks()
-        logger.info(f"Top five stocks sorted automatically {top_five_stocks}")
-        assert top_five_stocks == shared_top5_stocks, "The top5 stocks are not matching"
+        logger.info(f"Top five sorted automatically {top_five_stocks}")
+        assert top_five_stocks.keys() == shared_top5_stocks.keys(), "The top5 stocks are not matching"
